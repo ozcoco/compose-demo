828bba7d1b91bd39762d35bea156fa27ab368837
ozcomcn <857527916@qq.com>
2025-10-11 01:41:16 +0800

    标题栏内容实现

--- compose-beginner/src/main/java/com/ozcomcn/compose_beginner/components/AppBarsScreen.kt
+++ compose-beginner/src/main/java/com/ozcomcn/compose_beginner/components/AppBarsScreen.kt
@@ -1,24 +1,52 @@
 package com.ozcomcn.compose_beginner.components
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBarsScreen(
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(), content = {
-            Text(text = "AppBars")
-        })
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary,
+            ),
+            title = {
+                Text(text = "App Bars")
+            },
+            navigationIcon = {
+                IconButton(onClick = { }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null
+                    )
+                }
+            },
+        )
+        Spacer(modifier = Modifier.padding(top = 16.dp))
+    }
 }
--- compose-beginner/src/main/java/com/ozcomcn/compose_beginner/main/nav/MainNav.kt
+++ compose-beginner/src/main/java/com/ozcomcn/compose_beginner/main/nav/MainNav.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -92,9 +93,34 @@ fun MainNav(
                         )
                     )
                 }
+                HorizontalDivider(modifier = Modifier.padding(horizontal = 8.dp))
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            imageVector = if (isDarkTheme) Icons.Default.LightMode else Icons.Default.DarkMode,
+                            contentDescription = "主题模式"
+                        )
+                    }, label = { Text("主题模式") }, selected = false, onClick = {
+                            //
+                    }, modifier = Modifier.padding(
+                        start = 8.dp, end = 16.dp, top = 8.dp
+                    )
+                )
+                NavigationDrawerItem(
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Default.Settings,
+                            contentDescription = "设置"
+                        )
+                    }, label = { Text("设置") }, selected = false, onClick = {
+                           //
+                    }, modifier = Modifier.padding(
+                        start = 8.dp, end = 16.dp, top = 8.dp
+                    )
+                )
             }
         },
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
     ) {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
